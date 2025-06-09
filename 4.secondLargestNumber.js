1c1353dc7e57d0e182642ae9a338d4b2dd7fafff
Kartik Mishra <kartikmishra.km44@gmail.com>
2025-06-09 18:12:29 +0530

    corner cases handled

--- 4.secondLargestNumber.js
+++ 4.secondLargestNumber.js
@@ -1,6 +1,10 @@
 // write a function which returns the second largest number in an array
 
 function findSecondLargestNumber(arr) {
+  if (arr.length < 2) {
+    return "Array should have at least two elements";
+  }
+
   let largest = -Infinity;
   let secondLargest = -Infinity;
 
@@ -8,7 +12,7 @@ function findSecondLargestNumber(arr) {
     if (arr[i] > largest) {
       secondLargest = largest;
       largest = arr[i];
-    } else if (arr[i] > secondLargest) {
+    } else if (arr[i] > secondLargest && arr[i] != largest) {
       secondLargest = arr[i];
     }
   }
@@ -17,6 +21,7 @@ function findSecondLargestNumber(arr) {
 }
 
 console.log(findSecondLargestNumber([1, 2, 3, 4, 5, 6]));
+console.log(findSecondLargestNumber([1, 2, 3, 4, 5, 6, 10, 10]));
 console.log(findSecondLargestNumber([10, 2, 30, 20, 5, 6]));
 console.log(findSecondLargestNumber([-1, -2, -3, -40, -5, -60]));
 console.log(findSecondLargestNumber([-1, -2, -3, -40, 5, 6]));
